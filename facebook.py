ac4cb9c4b3bffb5c352c2aad20de93537151a1de
mriccitelli <62514610+mriccitelli@users.noreply.github.com>
2020-03-22 13:30:57 -0400

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,7 @@
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+
+if(userName.lower() == "mike" and userPassword == "bingo"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
